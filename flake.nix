8f4d4ed4926a1f4422015b3c5c3a153dc14f17e0
Nel <46218259+imNel@users.noreply.github.com>
2024-09-19 14:02:11 +0800

    removing a bunch of stuff. probably should commit

--- flake.nix
+++ flake.nix
@@ -182,53 +182,36 @@
                   # Hide the OS choice for bootloaders.
                   # It's still possible to open the bootloader list by pressing any key
                   # It will just not appear on screen unless a key is pressed
-                  loader.timeout = 0;
+                  # loader.timeout = 0;
 
                   # Enable "Silent Boot"
-                  consoleLogLevel = 0;
-                  initrd.verbose = false;
-                  kernelParams = [
-                    "quiet"
-                    "splash"
-                    "boot.shell_on_fail"
-                    "loglevel=3"
-                    "rd.systemd.show_status=false"
-                    "rd.udev.log_level=3"
-                    "udev.log_priority=3"
-                  ];
+                  # consoleLogLevel = 0;
+                  # initrd.verbose = false;
+                  # kernelParams = [
+                  #   "quiet"
+                  #   "splash"
+                  #   "boot.shell_on_fail"
+                  #   "loglevel=3"
+                  #   "rd.systemd.show_status=false"
+                  #   "rd.udev.log_level=3"
+                  #   "udev.log_priority=3"
+                  # ];
 
-                  plymouth = {
-                    enable = true;
-                    theme = "lone";
-                    themePackages = with pkgs; [
-                      # By default we would install all themes
-                      (adi1090x-plymouth-themes.override { selected_themes = [ "lone" ]; })
-                    ];
-                  };
+                  # plymouth = {
+                  #   enable = true;
+                  #   theme = "lone";
+                  #   themePackages = with pkgs; [
+                  #     # By default we would install all themes
+                  #     (adi1090x-plymouth-themes.override { selected_themes = [ "lone" ]; })
+                  #   ];
+                  # };
                 };
 
-                networking.hostName = "nixos"; # Define your hostname.
                 networking.networkmanager.enable = true;
 
                 # locale
                 time.timeZone = "Australia/Perth";
                 i18n.defaultLocale = "en_AU.UTF-8";
-                i18n.extraLocaleSettings = {
-                  LC_ADDRESS = "en_AU.UTF-8";
-                  LC_IDENTIFICATION = "en_AU.UTF-8";
-                  LC_MEASUREMENT = "en_AU.UTF-8";
-                  LC_MONETARY = "en_AU.UTF-8";
-                  LC_NAME = "en_AU.UTF-8";
-                  LC_NUMERIC = "en_AU.UTF-8";
-                  LC_PAPER = "en_AU.UTF-8";
-                  LC_TELEPHONE = "en_AU.UTF-8";
-                  LC_TIME = "en_AU.UTF-8";
-                };
-
-                # Enable the X11 windowing system.
-                services.xserver.enable = true;
-                services.xserver.displayManager.gdm.enable = true;
-                services.xserver.displayManager.defaultSession = "sway";
 
                 # Enable the sway window manager.
                 programs.sway = {
@@ -244,13 +227,10 @@
                     wmenu
                   ];
                 };
-                services.gnome.gnome-keyring.enable = true;
+                security.polkit.enable = true;
 
-                # Configure keymap in X11
-                services.xserver.xkb = {
-                  layout = "au";
-                  variant = "";
-                };
+                services.gnome.gnome-keyring.enable = true;
+                services.displayManager.defaultSession = "sway";
 
                 # Enable CUPS to print documents.
                 services.printing.enable = true;
@@ -290,12 +270,21 @@
                 };
                 programs.steam = {
                   enable = true;
-                  gamescopeSession.enable = true;
+                  # gamescopeSession.enable = true; # disable this for now, try opening gamescope from sway
                   remotePlay.openFirewall = true;
                   dedicatedServer.openFirewall = true;
                   localNetworkGameTransfers.openFirewall = true;
                 };
                 hardware.xone.enable = true;
+
+                hardware.bluetooth.enable = true;
+                hardware.bluetooth.powerOnBoot = true;
+                services.blueman.enable = true;
+
+                environment.systemPackages = with pkgs; [
+                  (import ./steam/switch-to-desktop.nix { inherit pkgs; })
+                  pavucontrol
+                ];
               };
             # nix-darwin specific configuration
             darwin =
