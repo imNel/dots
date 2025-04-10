03e4c4ec28c68513608ccee607b1450fb20ec58e
Nel Fernandes <46218259+imNel@users.noreply.github.com>
2025-04-10 23:14:56 +0800

    update fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,19 +1,21 @@
 set fish_greeting
 set -U EDITOR nvim
-set -g fish_key_bindings fish_vi_key_bindings
+fish_default_key_bindings
 
-fish_add_path "/opt/homebrew/bin/"
+fish_add_path /opt/homebrew/bin/
 
 alias lg="lazygit"
 alias ls="eza"
+alias l="ll"
+alias hpm="pnpm hpm"
 
 starship init fish | source
 zoxide init fish | source
 fnm env --use-on-cd --shell fish | source
 
 # pnpm
-set -gx PNPM_HOME "/Users/nel/Library/pnpm"
+set -gx PNPM_HOME /Users/nel/Library/pnpm
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
