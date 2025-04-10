9277e999c8c89c49bb979f9a13877b00020b63af
Nel Fernandes <46218259+imNel@users.noreply.github.com>
2025-04-10 23:44:39 +0800

    nvim: revert to default colorscheme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,25 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = 'tokyonight-storm'
+-- Color options
+config.color_scheme = "NvimDark"
+config.colors = {
+	tab_bar = {
+		background = "#14161B",
+		inactive_tab = {
+			bg_color = "#14161B",
+			fg_color = "#C4C6CD",
+		},
+		active_tab = {
+			bg_color = "#C4C6CD",
+			fg_color = "#14161B",
+		},
+	},
+}
+
 config.use_fancy_tab_bar = false
 config.show_new_tab_button_in_tab_bar = false
 config.window_decorations = "RESIZE"
-config.window_close_confirmation = 'NeverPrompt'
+config.window_close_confirmation = "NeverPrompt"
 
 return config
